8cda3e4daab09f6080c41bf8e1b0edd553a42507
Diogo Matsubara <diogo.matsubara@gmail.com>
2017-04-11 22:02:12 -0300

    implement solution to CRUD Model Methods (crud_spec.rb -e rq01): add a Racer.all method with multiple optinal parameters

--- app/models/racer.rb
+++ app/models/racer.rb
@@ -8,4 +8,12 @@ class Racer
     self.mongo_client['racers']
   end
 
+  def self.all(prototype={}, sort={:number=>1}, skip=0, limit=nil)
+    result = self.collection.find(prototype)
+                 .sort(sort)
+                 .skip(skip)
+    result = result.limit(limit) if !limit.nil?
+    return result
+  end
+
 end
